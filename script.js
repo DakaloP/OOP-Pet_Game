333875487d45ff9b0477232e46caf28a4e1c29fc
DakaloP <dam101@DAs-MacBook-Air-2.local>
2025-08-08 00:44:03 -0700

    i added two pets snake and rabbit

--- script.js
+++ script.js
@@ -1,73 +1,81 @@
-
-//     Object-Oriented Programming (OOP)
-
-
 class Pet {
-    constructor(name, sound, image) {
-      this.name = name;
-      this.sound = sound;
-      this.image = image;
-      this.playCount = 0;
-    }
-  
-    speak() {
-      alert(this.sound);
-    }
-  
-    play() {
-      this.playCount++;
-      this.speak();
-      updateCounter(this.playCount);
-      showFunActions(this.name);
-    }
+  constructor(name, sound, image) {
+    this.name = name;
+    this.sound = sound;
+    this.image = image;
+    this.playCount = 0;
   }
-  
-  // Global variable to store the chosen pet
-  let currentPet = null;
-  
-  //     Function to choose a pet
- 
-  function choosePet(type) {
-    if (type === 'dog') {
-      currentPet = new Pet('Dog', 'Woof! 🐶', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8x1-7sQlvgNwQSNaGYYqOTvAnyXNZo1wFqw&s');
-    } else {
-      currentPet = new Pet('Cat', 'Meow! 🐱', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4ZNGq9_b4Z0Gw-_HXeJLlI0wOPUJnT020Xg&s');
-    }
-  
+
+  speak() {
+    alert(this.sound);
+  }
+
+  play() {
+    this.playCount++;
+    this.speak();
+    updateCounter(this.playCount);
+    showFunActions(this.name);
+  }
+}
+
+// Global variable to store the chosen pet
+let currentPet = null;
+
+// Function to choose a pet
+function choosePet(type) {
+  if (type === 'dog') {
+    currentPet = new Pet('Dog', 'Woof! 🐶', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8x1-7sQlvgNwQSNaGYYqOTvAnyXNZo1wFqw&s');
+  } else if (type === 'cat') {
+    currentPet = new Pet('Cat', 'Meow! 🐱', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4ZNGq9_b4Z0Gw-_HXeJLlI0wOPUJnT020Xg&s');
+  } else if (type === 'snake') {
+    currentPet = new Pet('Snake', 'Hiss! 🐍', 'https://images.pexels.com/photos/1394938/pexels-photo-1394938.jpeg');
+  } else if (type === 'rabbit') {
+    currentPet = new Pet('Rabbit', 'Snore! 🐇', 'https://images.pexels.com/photos/20178218/pexels-photo-20178218.jpeg');
+  }
+
+  if (currentPet) {
     document.getElementById('petImage').src = currentPet.image;
     document.getElementById('game').style.display = 'block';
     updateCounter(0);
     showFunActions(currentPet.name);
   }
-  
-  //     Function to play with the pet
- 
-  function playWithPet() {
-    if (currentPet) {
-      currentPet.play();
-    }
+}
+
+// Function to play with the pet
+function playWithPet() {
+  if (currentPet) {
+    currentPet.play();
+  }
+}
+
+// Function to update the counter
+function updateCounter(count) {
+  document.getElementById('counter').innerText = `Times played: ${count}`;
+}
+
+// Function + Loop: Fun pet actions
+function showFunActions(petName) {
+  let actions = [];
+
+  switch (petName) {
+    case 'Dog':
+      actions = ['Bark', 'Wag Tail', 'Fetch Ball'];
+      break;
+    case 'Cat':
+      actions = ['Meow', 'Purr', 'Chase Mouse'];
+      break;
+    case 'Snake':
+      actions = ['Slither', 'Hiss', 'Hide in Rocks'];
+      break;
+    case 'Rabbit':
+      actions = ['Hop', 'Nibble Carrot', 'Dig Hole'];
+      break;
   }
 
-  //     Function to update the counter
+  const actionBox = document.getElementById('actions');
+  actionBox.innerHTML = '<strong>Fun Actions:</strong><br>';
 
-  function updateCounter(count) {
-    document.getElementById('counter').innerText = `Times played: ${count}`;
+  for (let i = 0; i < actions.length; i++) {
+    actionBox.innerHTML += '✅ ' + actions[i] + '<br>';
   }
-  
-
-  //     Function + Loop: Fun pet actions
-
-  function showFunActions(petName) {
-    const actions = petName === 'Dog'
-      ? ['Bark', 'Wag Tail', 'Fetch Ball']
-      : ['Meow', 'Purr', 'Chase Mouse'];
-  
-    const actionBox = document.getElementById('actions');
-    actionBox.innerHTML = '<strong>Fun Actions:</strong><br>';
-  
-    // Loop through actions and show them
-    for (let i = 0; i < actions.length; i++) {
-      actionBox.innerHTML += '✅ ' + actions[i] + '<br>';
-    }
-  }
-  
+}
