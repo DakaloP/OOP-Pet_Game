2fbb8511b91c521da85dd94b5605d08c63aa371e
DakaloP <dam101@DAs-MacBook-Air-2.local>
2025-08-11 09:20:55 -0700

    i added sound

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 class Pet {
-  constructor(name, soundText, image,soundUrl) {
+  constructor(name, soundText, image,soundurl) {
     this.name = name;
     this.soundText = soundText;
     this.image = image;
-    this.soundUrl = new Audio(soundUrl);//Audio
+    this.soundUrl = new Audio(soundurl)//Audio
     this.playCount = 0;
   }
 
   speak() {
     alert(this.soundText);
-    this.soundUrl.currentTime = 0; // ensures restart each click
+    this.soundUrl.currentTime = 0 // ensures restart each click
     this.soundUrl.play();
   }
 
@@ -18,7 +18,7 @@ class Pet {
     this.speak();
     updateCounter(this.playCount);
     showFunActions(this.name);
-    this.soundUrl.play;
+    this.soundurl.load();
   }
 }
 
@@ -30,18 +30,18 @@ function choosePet(type) {
   if (type === 'dog') {
     currentPet = new Pet('Dog', 'Woof! 🐶',
        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8x1-7sQlvgNwQSNaGYYqOTvAnyXNZo1wFqw&s',
-       'Media/small-dog-barking-381828.mp3');
+       'Media /Dog.mp3');
   } else if (type === 'cat') {
-    currentPet = new Pet('Cat', 'Meow! 🐱','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4ZNGq9_b4Z0Gw-_HXeJLlI0wOPUJnT020Xg&s',
-      'Media/cat-meow-sound-383823.mp3'
+    currentPet = new Pet('Cat', 'Meow! 🐱', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4ZNGq9_b4Z0Gw-_HXeJLlI0wOPUJnT020Xg&s',
+      'Media /Cat.mp3'
     );
   } else if (type === 'snake') {
-    currentPet = new Pet('Snake', 'Hiss! 🐍','https://images.pexels.com/photos/1394938/pexels-photo-1394938.jpeg',
-      'Media/snake-hiss-95241.mp3'
+    currentPet = new Pet('Snake', 'Hiss! 🐍', 'https://images.pexels.com/photos/1394938/pexels-photo-1394938.jpeg',
+      'Media /Snake.mp3'
     );
   } else if (type === 'rabbit') {
-    currentPet = new Pet('Rabbit', 'Snore! 🐇','https://images.pexels.com/photos/20178218/pexels-photo-20178218.jpeg',
-      'Media/rabbit-sounds-358172.mp3'
+    currentPet = new Pet('Rabbit', 'Snore! 🐇', 'https://images.pexels.com/photos/20178218/pexels-photo-20178218.jpeg',
+      'Media /Rabbit.mp3'
     );
   }
 
